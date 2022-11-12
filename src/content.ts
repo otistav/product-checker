8b7d80d4f3bcd350e75f50579d493d9d39f30c09
Sam Bocharov <semen_bard@mail.ru>
2022-11-12 19:19:00 +0200

    enabling and disabling app with shortcut

--- src/content.ts
+++ src/content.ts
@@ -1,7 +1,39 @@
 import constants, { HoverStyle } from "./utils/constants";
 
+type State = {
+  applicationEnabled: boolean;
+  metaPressed: boolean;
+  helperPressed: boolean;
+}
+
+let state: State = {
+  applicationEnabled: false,
+  metaPressed: false,
+  helperPressed: false,
+}
+
 const onMaskClick = (e: MouseEvent) => {
   console.log(e, 'event');
+}
+
+const onKeyDown = (e: KeyboardEvent, state: State): State => {
+  let newState = { ...state };
+  if (e.key === constants.COMMAND_KEY) newState = { ...state, metaPressed: true };
+  if (e.key === constants.HELPER_KEY) newState = { ...state, helperPressed: true };
+  if (newState.metaPressed && newState.helperPressed) {
+    const hoverMask = document.getElementById(constants.HOVER_ID);
+    if (state.applicationEnabled && hoverMask) {
+      hoverMask.parentElement?.removeChild(hoverMask);
+    }
+    newState = { ...state, applicationEnabled: !state.applicationEnabled };
+  }
+  return newState;
+}
+
+const onKeyUp = (e: KeyboardEvent, state: State): State => {
+  if (e.key === constants.COMMAND_KEY) return { ...state, metaPressed: false };
+  if (e.key === constants.HELPER_KEY) return { ...state, helperPressed: false };
+  return state;
 }
 
 const createMask = (target: Element) => {
@@ -41,5 +73,6 @@ const handleElementUnderMouse = (e: MouseEvent, applicationEnabled: boolean) => 
   }
 }
 
-document.addEventListener('mousemove', (e) => handleElementUnderMouse(e, true));
-
+document.addEventListener('mousemove', (e) => handleElementUnderMouse(e, state.applicationEnabled));
+document.addEventListener('keydown', (e) => state = onKeyDown(e, state));
+document.addEventListener('keyup', (e) => state = onKeyUp(e, state));
